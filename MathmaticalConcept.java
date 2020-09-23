d797128be8411e8ac508e97b660a7ad58fb2f2f0
vimalraina <vimalraina@gmail.com>
2020-09-23 10:54:51 +0530

    Create MathmaticalConcept.java

---
+++ MathmaticalConcept.java
@@ -0,0 +1,24 @@
+package JavaTraining;
+
+public class MathmaticalConcept {
+
+	public static void main(String[] args) {
+		
+		int i = 10/2;
+		System.out.println(i);
+		
+		int j = 9/2;
+		System.out.println(j);
+		
+		System.out.println(11/2.0);
+		
+		float d = 9/2f;
+		System.out.println(d);
+		
+		int k = 9/0;
+		System.out.println(k);	
+				
+
+	}
+
+}
